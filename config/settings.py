f5c709f1221f369201661345a79fcbdc41f5f0ac
Monir <monirulslm7@gmail.com>
2024-09-12 15:47:26 +0600

    add timezone configure done.

--- config/settings.py
+++ config/settings.py
@@ -141,12 +141,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'Asia/Dhaka'
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
+FORCE_STATIC_FILE_SERVING = True
 
 
 # Static files (CSS, JavaScript, Images)
