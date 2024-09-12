0e7acc03b412d073d951df523ddb583532d68d98
Monir <monirulslm7@gmail.com>
2024-09-12 15:43:05 +0600

    requirements file updated.

--- config/settings.py
+++ config/settings.py
@@ -69,11 +69,49 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+""" 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+
+# DATAB BASE CREDENTIALS.
+DB_NAME = 'e_shoper'
+DB_USER = 'postgres'
+DB_PASS = 'Ksy12345'
+DB_HOST = '127.0.0.1'
+DB_PORT = '5432'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASS,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
+    }
+}
+
+REDIS_HOST = 'redis'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://{}:6379/1".format(REDIS_HOST),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "SOCKET_CONNECT_TIMEOUT": 5,
+            "SOCKET_TIMEOUT": 5,
+            "IGNORE_EXCEPTIONS": True,
+            "SERIALIZER": "django_redis.serializers.json.JSONSerializer",
+            "MASTER_CACHE": f"redis://{REDIS_HOST}:6379",
+            "DB": 4,
+        },
+        "KEY_PREFIX": "core",
     }
 }
 
